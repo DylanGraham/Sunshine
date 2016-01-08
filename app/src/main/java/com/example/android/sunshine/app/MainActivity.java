51102d4e8a2c08dff5967686655257d64b1b3924
Dylan Graham <dylan.graham@gmail.com>
2016-01-08 13:53:01 +0800

    Added detailfragment share menu. Need to add action bar to this layout

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -14,7 +14,7 @@ import android.view.MenuItem;
 public class MainActivity extends ActionBarActivity {
 
     private final String LOG_TAG = MainActivity.class.getSimpleName();
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
